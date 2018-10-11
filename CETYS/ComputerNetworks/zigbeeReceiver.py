369abb2366d534fce60e0ea6603f87920c6152da
MiguelFrias <miguel.frias@cetys.edu.mx>
2018-10-10 17:39:55 -0700

    zigbee

--- CETYS/ComputerNetworks/zigbeeReceiver.py
+++ CETYS/ComputerNetworks/zigbeeReceiver.py
@@ -1,10 +1,10 @@
 import serial
 import time
 
-ser = serial.Serial('dev/ttyUSB0', 9600, timeout=3)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=3)
 print('connecting Serial Port')
 
 while True:
-	print ('Waiting...')
+#	print ('Waiting...')
 	line = ser.readline().strip()
 	print(line)
