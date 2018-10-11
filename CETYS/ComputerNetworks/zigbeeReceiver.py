4afcdd05707737d9255a76c01b5601de073d6887
MiguelFrias <miguel.frias@cetys.edu.mx>
2018-10-10 17:33:01 -0700

    zigbee

---
+++ CETYS/ComputerNetworks/zigbeeReceiver.py
@@ -0,0 +1,10 @@
+import serial
+import time
+
+ser = serial.Serial('dev/ttyUSB0', 9600, timeout=3)
+print('connecting Serial Port')
+
+while True:
+	print ('Waiting...')
+	line = ser.readline().strip()
+	print(line)
