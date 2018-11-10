b45cd2763b1d4e232abf583efa121a2a5a6ab658
MiguelFrias <miguel.frias@cetys.edu.mx>
2018-11-09 16:54:10 -0800

    Uploading digital control files

--- CETYS/DigitalControl/squareSignal.py
+++ CETYS/DigitalControl/squareSignal.py
@@ -1,18 +1,19 @@
+#from scipy import signal
+import numpy as np
 import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(12,gpio.OUT)
-gpio.setwarnings(False)
-
 while True:
+	gpio.cleanup()
 	gpio.setmode(gpio.BOARD)
 	gpio.setup(12,gpio.OUT)
+	gpio.setwarnings(False)
+
 	p = gpio.PWM(12,1)
 	p.start(10)
-	time.sleep(2)
-	p.stop()
-	gpio.cleanup()
-	time.sleep(5)
-
-
+	time.sleep(10)
+	#raw_input('')
+#	gpio.cleanup()
+#	time.sleep(10)
