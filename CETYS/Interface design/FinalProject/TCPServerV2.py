8149085f9f8376add6de5c8ac4bdcc185a6aeeb2
MiguelFrias <miguel.frias@cetys.edu.mx>
2018-11-27 17:16:23 -0800

    Testing sockets

---
+++ CETYS/Interface design/FinalProject/TCPServerV2.py
@@ -0,0 +1,35 @@
+import socket
+
+
+#TCP_IP = ''
+#TCP_PORT = 9000
+#BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.bind((TCP_IP, TCP_PORT))
+#s.listen(1)
+
+#conn, addr = s.accept()
+#print 'Connection address:', addr
+
+lock = True
+while lock:
+	TCP_IP = ''
+	TCP_PORT = 9000
+	BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((TCP_IP, TCP_PORT))
+	s.listen(1)
+
+	conn, addr = s.accept()
+	print 'Connection address:', addr
+	while 1:
+    		data = conn.recv(BUFFER_SIZE)
+    		if not data: break
+    		print "received data:", data
+    		conn.send(data)  # echo
+	if data == 'h':
+		lock = False
+
+conn.close()
